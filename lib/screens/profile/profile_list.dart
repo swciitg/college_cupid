99e648aa6271494747522d87968d6cd27b63fc9e
vikdah <dahvik546@gmail.com>
2023-10-01 22:09:16 +0530

    Profile page of the user and other users that we will see are implemented

--- lib/screens/profile/profile_list.dart
+++ lib/screens/profile/profile_list.dart
@@ -1,22 +1,20 @@
-import 'package:college_cupid/screens/profile/profile_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expandable_text/expandable_text.dart';
-
+import 'package:cupertino_icons/cupertino_icons.dart';
 import '../../shared/colors.dart';
 
-class ProfileTab extends StatefulWidget {
-  const ProfileTab({super.key});
+class ProfileList extends StatefulWidget {
+  const ProfileList({super.key});
 
   @override
-  State<ProfileTab> createState() => _ProfileTabState();
+  State<ProfileList> createState() => _ProfileListState();
 }
 
-class _ProfileTabState extends State<ProfileTab> {
+class _ProfileListState extends State<ProfileList> {
 
+  bool read_more = false;
 
-   bool read_more = false;
-
-  Widget editButton(var w){
+  Widget likeButton(var w){
     return Container(
       height: w/5,
       width: w/5,
@@ -25,18 +23,38 @@ class _ProfileTabState extends State<ProfileTab> {
         color: Colors.pinkAccent,
       ),
       child: GestureDetector(
-        child:Icon(Icons.edit,size: w/8,color: Colors.white,),
+        child:Icon(Icons.favorite,size: w/8,color: Colors.white,),
         onTap: () {
-           Navigator.push(context, MaterialPageRoute(builder:  (context) => ProfileList()));
+          // like function for your crush
         },
       )
     );
   }
 
 
+    Widget backButton(var w){
+    return Container(
+      height: w/8,
+      width: w/8,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(w/16)),
+        color: const Color.fromARGB(255, 208, 199, 199),
+      ),
+      child: GestureDetector(
+        child:Center(child: Padding(
+          padding: const EdgeInsets.only(left: 9.7),
+          child: Icon(Icons.arrow_back_ios,size: w/12,color: Colors.white,),
+        )),
+        onTap: () {
+            // go back function 
+        },
+      )
+    );
+  }
+
   Widget gridView(){
     return Container(
-      height: 80,
+      height: 100,
       padding: EdgeInsets.all(8),
       child: GridView.count(
         crossAxisCount: 3,
@@ -45,7 +63,6 @@ class _ProfileTabState extends State<ProfileTab> {
         mainAxisSpacing: 10,
         children: List.generate(5, (index) {
           return Container(
-            // height: 20,
             decoration: BoxDecoration(
               color: Colors.white,
               boxShadow: [
@@ -63,6 +80,12 @@ class _ProfileTabState extends State<ProfileTab> {
       ),
     );
   }
+
+  
+
+
+
+
 
   Widget aboutMe(var h){
     return Container(
@@ -102,20 +125,20 @@ class _ProfileTabState extends State<ProfileTab> {
                 Padding(padding: EdgeInsets.only(top: 8)),
                 Text("About",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),textAlign: TextAlign.left,),
                 Padding(padding: EdgeInsets.only(top: 8)),
-                
+                // Text(),
                Container(
-                        height: read_more ? 33 : null,
+                        height: !read_more ? 33 : null,
                         child: Text("About me RichText Widget: Allows you to style different parts of the text differently. "
-                     "It uses the TextSpan widget to define styled spans of text within the widget.",overflow: TextOverflow.clip),
+                     "It uses the TextSpan widget to define styled spans of text within the widget.",overflow: TextOverflow.clip,),
                 ),
-                  
                 Padding(padding: EdgeInsets.only(top: 8)),
                 GestureDetector(
-                  child: Text(!read_more ? "Show less":"Read more",style: TextStyle(color: CupidColors.navBarIconColor),textAlign: TextAlign.left,),
+                  child: Text( read_more ? "Show less":"Read more",style: TextStyle(color: CupidColors.navBarIconColor),textAlign: TextAlign.left,),
                   onTap: () {
-                      setState(() {
-                        read_more = !read_more;
-                      });
+                    setState(() {
+                      read_more = !read_more;
+                    });
+                    
                   },
                 ),
                 Padding(padding: EdgeInsets.only(top: 8)),
@@ -134,19 +157,33 @@ class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     var safeArea = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Container(
+     return 
+    Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+       body:
+       SingleChildScrollView(
+        child: Container(
+        // height: double.maxFinite,
         child: Stack(
           children: [
+            Padding(padding: EdgeInsets.only(top: 10)),
             aboutMe(safeArea.height),
             Positioned(
                 left: 2*safeArea.width/5,
                 top: safeArea.height/2-safeArea.width/10,
-                child: editButton(safeArea.width)
+                child: likeButton(safeArea.width)
             ),
+            Positioned(
+              left: safeArea.width/12,
+              top: safeArea.width/12,
+              child: backButton(safeArea.width))
           ],
         ),
-      )
+      ),
+      ),
     );
   }
 }
