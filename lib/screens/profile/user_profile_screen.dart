2df59fd5543a7e32e2bff28367b8f420c5102a65
Hareesh-Nandigrama <hareeshnandigrama@gmail.com>
2024-01-07 16:20:15 +0530

    fixed ui bug

--- lib/screens/profile/user_profile_screen.dart
+++ lib/screens/profile/user_profile_screen.dart
@@ -91,99 +91,103 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
           color: Colors.white,
         ),
       ),
-      body: SingleChildScrollView(
-          child: Stack(
-        children: [
-          Positioned(
-              child: CachedNetworkImage(
-            imageUrl: widget.userProfile.profilePicUrl,
-            progressIndicatorBuilder: (context, url, progress) =>
-                CircularProgressIndicator(
-              value: progress.progress,
-            ),
-          )),
-          Column(
-            children: [
-              SizedBox(
-                height: safeArea.width * 0.9,
-              ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                decoration: const BoxDecoration(
-                    color: CupidColors.backgroundColor,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20))),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.userProfile.name,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 25),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Row(
-                        children: [
-                          Text(
-                            widget.userProfile.email,
+      body: SafeArea(
+        child: SingleChildScrollView(
+            child: Stack(
+          children: [
+            Positioned(
+                child: CachedNetworkImage(
+                              imageUrl: widget.userProfile.profilePicUrl,
+                              width: safeArea.width,
+                              fit: BoxFit.fill,
+                              progressIndicatorBuilder: (context, url, progress) =>
+                  CircularProgressIndicator(
+                value: progress.progress,
+                              ),
+                            )),
+            Column(
+              children: [
+                SizedBox(
+                  height: safeArea.width * 0.9,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                      color: CupidColors.backgroundColor,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.userProfile.name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              widget.userProfile.email,
+                              textAlign: TextAlign.left,
+                            ),
+                            const Expanded(child: SizedBox()),
+                            Text(
+                              "${Program.values.firstWhere((p) => p.databaseString == widget.userProfile.program).displayString} '${widget.userProfile.yearOfJoin}",
+                              textAlign: TextAlign.right,
+                            )
+                          ],
+                        ),
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 8)),
+                      const Text(
+                        'About',
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        textAlign: TextAlign.left,
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 8)),
+                      SizedBox(
+                        height: readMore ? 33 : null,
+                        child: Text(widget.userProfile.bio,
+                            overflow: TextOverflow.clip),
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 8)),
+                      if (widget.userProfile.bio.length > 30)
+                        GestureDetector(
+                          child: Text(
+                            !readMore ? "Show less" : "Read more",
+                            style: const TextStyle(
+                                color: CupidColors.navBarIconColor),
                             textAlign: TextAlign.left,
                           ),
-                          const Expanded(child: SizedBox()),
-                          Text(
-                            "${Program.values.firstWhere((p) => p.databaseString == widget.userProfile.program).displayString} '${widget.userProfile.yearOfJoin}",
-                            textAlign: TextAlign.right,
-                          )
-                        ],
-                      ),
-                    ),
-                    const Padding(padding: EdgeInsets.only(top: 8)),
-                    const Text(
-                      'About',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                      textAlign: TextAlign.left,
-                    ),
-                    const Padding(padding: EdgeInsets.only(top: 8)),
-                    SizedBox(
-                      height: readMore ? 33 : null,
-                      child: Text(widget.userProfile.bio,
-                          overflow: TextOverflow.clip),
-                    ),
-                    const Padding(padding: EdgeInsets.only(top: 8)),
-                    if (widget.userProfile.bio.length > 30)
-                      GestureDetector(
-                        child: Text(
-                          !readMore ? "Show less" : "Read more",
-                          style: const TextStyle(
-                              color: CupidColors.navBarIconColor),
-                          textAlign: TextAlign.left,
+                          onTap: () {
+                            setState(() {
+                              readMore = !readMore;
+                            });
+                          },
                         ),
-                        onTap: () {
-                          setState(() {
-                            readMore = !readMore;
-                          });
-                        },
+                      const Padding(padding: EdgeInsets.only(top: 8)),
+                      const Text(
+                        "Interests",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
                       ),
-                    const Padding(padding: EdgeInsets.only(top: 8)),
-                    const Text(
-                      "Interests",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.left,
-                    ),
-                    const Padding(padding: EdgeInsets.only(top: 8)),
-                    gridView(),
-                  ],
+                      const Padding(padding: EdgeInsets.only(top: 8)),
+                      gridView(),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
-      )),
+              ],
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
